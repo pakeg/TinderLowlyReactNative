56ebb002f271bfc3e1dba4aff3bdf1e1b81ff9d4
Pakeg Bae <pakegpakeg@gmail.com>
2023-05-20 22:50:49 +0000

    Update 13 files

--- TinderLowly/components/StackNavigator.js
+++ TinderLowly/components/StackNavigator.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import ChatScreen from "../screens/ChatScreen";
 import LoginScreen from "../screens/LoginScreen";
+import ModalScreen from "../screens/ModalScreen";
 import useAuth from "../hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
@@ -14,16 +15,26 @@ const StackNavigator = () => {
     <Stack.Navigator>
       {user ? (
         <>
-          <Stack.Screen
-            name="HomeScreen"
-            options={{ headerShown: false }}
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="ChatScreen"
-            options={{ headerShown: false }}
-            component={ChatScreen}
-          />
+          <Stack.Group>
+            <Stack.Screen
+              name="HomeScreen"
+              options={{ headerShown: false }}
+              component={HomeScreen}
+            />
+
+            <Stack.Screen
+              name="ChatScreen"
+              options={{ headerShown: false }}
+              component={ChatScreen}
+            />
+          </Stack.Group>
+          <Stack.Group>
+            <Stack.Screen
+              name="ModalScreen"
+              options={{ headerShown: false, presentation: "modal" }}
+              component={ModalScreen}
+            />
+          </Stack.Group>
         </>
       ) : (
         <Stack.Screen
